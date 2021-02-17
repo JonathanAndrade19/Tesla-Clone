28421f67324a3023b2091c5df36243b85067c94f
Jonathan <jonathanjampamb@gmail.com>
2021-02-17 11:42:25 -0300

    creating component in project

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { ModelsWrapper, ModelSection } from '../Model'
+import DefaultOverLayContent from '../DefaultOverLayContent';
 
 import { Container } from './styles';
 
 const Page: React.FC = () => {
   return (
     <Container>
-      <h1>Page</h1>
+      <ModelsWrapper>
+        <div>
+          <ModelSection
+            className="colored"
+            modelName="Model One"
+            overlayNode={
+              <DefaultOverLayContent
+                label="Model One"
+                description="Order Online for Delivery"
+              />
+            }
+          />
+            
+        </div>
+      </ModelsWrapper>
     </Container>
   );
 };
--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    .colored:nth-child(1) {
+        background: #f1ffe7;
 
+    }
+    .colored:nth-child(2) {
+        background: #00FFFF;
+
+    }
+    .colored:nth-child(3) {
+        background: #F5F5DC;
+
+    }
+    .colored:nth-child(4) {
+        background: #008B8B;
+
+    }
+    .colored:nth-child(5) {
+        background: #B8860B;
+
+    }
+    .colored:nth-child(6) {
+        background: #556B2F;
+
+    }
+    .colored:nth-child(7) {
+        background: #9932CC;
+
+    }
 `;
